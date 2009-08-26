808c39ffd2d76ff9b50e89144e984aa456d33ad2
Ricardo Ruiz <ricardo@tecnologia2.fabrica.locaweb.com.br>
2009-08-25 21:09:57 -0300

    refatorando parser de cli para fora da lib

--- lib/communigate/cli_parser.rb
+++ lib/communigate/cli_parser.rb
@@ -1,5 +1,222 @@
+require 'communigate/data_exception'
+require 'communigate/data_block'
+require 'ipaddr'
+
 module CommuniGate
   class CliParser
+    DATETIME_FORMAT = "#T%d-%m-%Y_%H:%M:%S" 
+    DATE_FORMAT = "#T%d-%m-%Y"
     
+    def initialize data
+      @data = data
+      @marker = 0
+    end
+
+    def self.to_cgp data
+      parser = self.new data
+      parser.ruby_to_cg
+    end
+    
+    def self.to_ruby data
+      parser = self.new data
+      parser.parse_cli
+    end
+
+    def parse_cli
+      _skip_ws
+      c = @data[@marker, 1]
+      case c
+      when /\(/
+        @marker += 1; return _ruby_array
+      when /\{/
+        @marker += 1; return _ruby_hash
+      when /\#/
+        if @data[@marker, 3] =~ /\#-*\d/ || @data[@marker, 2] =~ /\#\d/
+          @marker += 1
+          return _ruby_numeric
+        end
+        if @data[@marker, 2] =~ /#T/
+          @marker += 2
+          return _ruby_time
+        end
+        if @data[@marker, 2] =~ /#I/
+          @marker += 2
+          return _ruby_string
+        end
+      else
+        return _ruby_string
+      end
+    end
+
+    def ruby_to_cg (output = @data)
+      ret_str = String.new
+      if output.class == String && output.to_s.empty?
+        return '""'
+      elsif output.class == Hash
+        ret_str << '{'
+        output.each { |key,value| ret_str << ruby_to_cg(key) << '=' << ruby_to_cg(value) << ';' }
+        return ret_str + '}'
+      elsif output.class == Array
+        ret_str << '('
+        output.each_index { |idx| ret_str << ',' if idx > 0; ret_str << ruby_to_cg(output[idx]) }
+        return ret_str + ')'
+      elsif output.class == IPAddr
+        return "#I[#{output.to_s}]"
+      elsif output.class == Time || output.class == DateTime
+        return output.strftime(DATETIME_FORMAT)
+      elsif output.class == Date
+        return output.strftime(DATE_FORMAT)
+      elsif output.is_a?(Numeric)
+        return "##{output.to_s}"
+      elsif output.is_a?(CommuniGate::DataBlock)
+        return "[#{output.datablock}]"
+      else
+        output = output.to_s if output.class == Symbol
+        if output =~ /^[A-Za-z0-9]+$/
+          return output
+        end
+      
+        if output =~ /([[:cntrl:]])/
+          output.gsub!(/([[:cntrl:]])/m){ |i| "\\" + i[0].to_s.rjust(3,"0"); }
+          return %Q{"#{output}"}
+        else
+          return output.inspect
+        end
+      end
+    end
+    
+    protected
+    def _raise_data_exp(expecting='')
+      old_data = @data
+      old_marker = @marker
+      @data = ''
+      @marker = 0
+      raise CommuniGate::DataException.new(old_data,old_marker,expecting)
+    end
+    
+    def _skip_ws
+      while @data[@marker, 1] =~ /\s/
+        @marker += 1
+      end
+    end
+
+    def _ruby_time
+      remain_len = 10 #date w/ no time
+      if @data[@marker + remain_len, 1] == '_'
+        remain_len = 19
+      end
+      ret_time = Time.gm(*Time.parse(@data[@marker, remain_len]))
+      @marker += remain_len
+      return ret_time
+    end
+
+    def _ruby_numeric
+      _skip_ws
+      remain_len = /([-\d\.]+)/.match(@data[@marker, @data.length - @marker])[0].length
+      if /\./.match(@data[@marker, remain_len])
+        ret_num = @data[@marker, remain_len].to_f
+      else
+        ret_num = @data[@marker, remain_len].to_i
+      end
+      @marker += remain_len
+      return ret_num
+    end
+
+
+    def _ruby_string
+      _skip_ws
+      quoted = false
+      data_block = false
+      c = String.new
+      ret_string = String.new
+      if /\"/.match(@data[@marker, 1]) 
+        quoted = true
+        @marker += 1
+      elsif /\[/.match(@data[@marker, 1])
+        @marker += 1
+        rest_string = @data[@marker, @data.length-1];
+        the_end = rest_string =~ /\]/;
+        ret_string = @data[@marker, the_end].unpack('m')[0]
+        @marker += the_end + 1;
+        return ret_string
+      end
+
+      while @marker < @data.length
+        c = @data[@marker, 1]
+        if quoted #quoted string
+          if c == '\\'
+            if /(?:\"|\\|\d\d\d)/.match(@data[@marker, 3])
+              @marker += 1
+              c = @data[@marker, 3]
+              if /\d\d\d/.match(c)
+                @marker += 2
+                c=c.to_i.chr
+              elsif c == "\""
+                c += "\\\""
+              else
+                c='\\'+c[0,1]
+              end
+            end
+          elsif c == '"'
+            @marker += 1
+            _skip_ws
+            if @data[@marker, 1] == '"'
+              ++@marker
+            else
+              break
+            end
+          end
+        elsif /[-a-zA-Z0-9\x80-\xff_\.\@\!\#\%\:]/.match(c)
+        else
+          break
+        end
+        ret_string << c
+        @marker += 1
+      end
+      return ret_string
+    end
+
+    def _ruby_array
+      ret_array = Array.new
+      while @marker < @data.length
+        _skip_ws
+        if @data[@marker, 1] == ')'
+          @marker += 1
+          break
+        else
+          ret_array.push(_parse_output)
+          _skip_ws
+          if @data[@marker, 1] == ','
+            @marker += 1
+          elsif @data[@marker, 1] == ')'
+            next
+          else
+            _raise_data_exp(', or )')
+          end
+        end	
+      end
+      return ret_array
+    end
+
+    def _ruby_hash
+      ret_hash = Hash.new
+      while @marker < @data.length
+        _skip_ws
+        if @data[@marker, 1] == '}'
+          @marker += 1
+          break
+        else
+          key = _ruby_string
+          _skip_ws
+          _raise_data_exp('=') unless @data[@marker, 1] == '='
+          @marker += 1
+          _skip_ws
+          ret_hash[key] = _parse_output
+          _raise_data_exp(';') unless @data[@marker, 1] == ';'
+          @marker += 1
+        end
+      end
+      return ret_hash
+    end
   end
 end
--- spec/ruby_to_cgp_spec.rb
+++ spec/ruby_to_cgp_spec.rb
@@ -3,38 +3,38 @@ require File.dirname(__FILE__) + '/spec_helper'
 context "CommuniGate CLI library from Ruby to CGP" do
   describe :numerics do    
     it "should convert ruby numerics to CGP numbers" do
-      CommuniGate::Cli._ruby_to_cg(1).should == '#1'
-      CommuniGate::Cli._ruby_to_cg(-1).should == '#-1'
-      CommuniGate::Cli._ruby_to_cg(-3.14159) == '#-3.14159'
+      CommuniGate::CliParser.to_cgp(1).should == '#1'
+      CommuniGate::CliParser.to_cgp(-1).should == '#-1'
+      CommuniGate::CliParser.to_cgp(-3.14159) == '#-3.14159'
     end
   end
   
   describe "control chars" do
     it "should convert to CGP escaped" do
-      CommuniGate::Cli._ruby_to_cg("a\tstring\twith\ttabs\0").should == 
+      CommuniGate::CliParser.to_cgp("a\tstring\twith\ttabs\0").should == 
         %q{"a\009string\009with\009tabs\000"}
     end
   end
   
   describe "IP Addresses" do
     it "should convert IPAddr objects to CGP ips" do
-      CommuniGate::Cli._ruby_to_cg(IPAddr.new('192.168.1.1')).should ==
+      CommuniGate::CliParser.to_cgp(IPAddr.new('192.168.1.1')).should ==
         '#I[192.168.1.1]'
     end
   end
   
   describe :strings do
     it "should convert simple strings to unquoted strings" do
-      CommuniGate::Cli._ruby_to_cg("string").should == "string"
+      CommuniGate::CliParser.to_cgp("string").should == "string"
     end
     
     it "should convert longer strings to quoted strings" do
-      CommuniGate::Cli._ruby_to_cg("longer string").should ==
+      CommuniGate::CliParser.to_cgp("longer string").should ==
         %q{"longer string"}
     end
     
     it "should convert quoted strings to escaped quoted strings" do
-      CommuniGate::Cli._ruby_to_cg("a \"quoted\" string").should == 
+      CommuniGate::CliParser.to_cgp("a \"quoted\" string").should == 
         %q{"a \"quoted\" string"}
     end
   end
@@ -42,42 +42,42 @@ context "CommuniGate CLI library from Ruby to CGP" do
   describe :time do
     it "should convert Time objects to CGP timestamps" do
       t = DateTime.new(2002, 10, 20, 07, 45, 30)
-      CommuniGate::Cli._ruby_to_cg(t).should == "#T20-10-2002_07:45:30"
+      CommuniGate::CliParser.to_cgp(t).should == "#T20-10-2002_07:45:30"
     end
     
     it "should convert DateTime objects to CGP timestamps" do
       t = Time.local(2002, 10, 20, 07, 45, 30)
-      CommuniGate::Cli._ruby_to_cg(t).should == "#T20-10-2002_07:45:30"
+      CommuniGate::CliParser.to_cgp(t).should == "#T20-10-2002_07:45:30"
     end
     
     it "should convert Time objects to CGP dates" do
       t = Date.new(2002, 10, 20)
-      CommuniGate::Cli._ruby_to_cg(t).should == "#T20-10-2002"
+      CommuniGate::CliParser.to_cgp(t).should == "#T20-10-2002"
     end
     
   end
   
   describe :arrays do  
     it "should convert emtpy ruby arrays to empty CGP arrays" do
-      CommuniGate::Cli._ruby_to_cg([]).should == "()"
+      CommuniGate::CliParser.to_cgp([]).should == "()"
     end
 
     it "should convert ruby arrays to CGP arrays" do 
-      CommuniGate::Cli._ruby_to_cg([1]).should == "(#1)"
-      CommuniGate::Cli._ruby_to_cg([-11, "string", "longer string"]) ==
+      CommuniGate::CliParser.to_cgp([1]).should == "(#1)"
+      CommuniGate::CliParser.to_cgp([-11, "string", "longer string"]) ==
         '(#-11, string, "longer string")'
     end
   end
   
   describe :hashes do
     it "should convert empty ruby Hashes to CGP dictionaries" do
-      CommuniGate::Cli._ruby_to_cg({}).should == "{}"
+      CommuniGate::CliParser.to_cgp({}).should == "{}"
     end
     
     it "should convert ruby Hashes to CGP dictionaries" do
       input = {:keysymbol => "string", :secondsymbol => "long string"}
-      CommuniGate::Cli._ruby_to_cg(input).should ==
-        "{secondsymbol=\"long string\";keysymbol=string;}"
+      CommuniGate::CliParser.to_cgp(input).should include "secondsymbol=\"long string\";"
+      CommuniGate::CliParser.to_cgp(input).should include "keysymbol=string;"
     end
     
     it "should convert complex ruby Hashes to CGP dictionaries" do
@@ -99,7 +99,7 @@ context "CommuniGate CLI library from Ruby to CGP" do
         "\"e_is_a_hash\"={key=value;};",
         "h=#T23-01-2012_10:45:20",
       ]
-      result = CommuniGate::Cli._ruby_to_cg(complex_hash)
+      result = CommuniGate::CliParser.to_cgp(complex_hash)
       expected_parts.each { |part| result.should include part }
     end
   end    
